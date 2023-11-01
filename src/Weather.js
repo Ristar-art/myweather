1495b26122ad10d480d422b0b8b2e7ead9cd99a0
Ristar-art <mochochokoboiketlo@gmail.com>
2023-11-01 17:07:28 +0200

    API conected

--- src/Weather.js
+++ src/Weather.js
@@ -2,12 +2,58 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Weather = () => {
+  const [weatherData, setWeatherData] = useState({});
+  const [error, setError] = useState(null);
+   const [location,setLocation] = useState("")
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,precipitation_probability_max,windspeed_10m_max&timezone=auto"
+        );
+        setWeatherData(response.data);
+      } catch (error) {
+        console.error("Error fetching weather data: ", error);
+      }
+    };
+    fetchData();
+    const fetchLocation = async (latitude, longitude) => {
+        try {
+          const response = await axios.get(
+            `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`
+          );
+          setLocation(response.data.display_name);
+        } catch (error) {
+          console.error("Error fetching location data: ", error);
+        }
+      };
+    const successCallback = (position) => {
+        const { latitude, longitude } = position.coords;
+        fetchLocation(latitude, longitude);
+      };
+  
+      const errorCallback = (error) => {
+        console.error(error.message);
+      };
+  
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+      }
+  }, []);
+
+  if (error) {
+    return <div>Error fetching weather data: {error.message}</div>;
+  }
+ 
+
   return (
     <>
       <div
         style={{
           width: "60%",
-          height: "80%",
+          height: "90%",
           display: "flex",
           flexDirection: "row",
           justifyContent: "flex-start",
@@ -29,11 +75,11 @@ const Weather = () => {
           <div
             style={{
               width: "100%",
-              height: "10%",
+              height: "30%",
               display: "flex",
               flexDirection: "row",
               justifyContent: "center",
-              //   backgroundColor: "red",
+              //  backgroundColor: "yellow",
               borderRadius: 20,
               alignItems: "center",
               // paddingLeft:20
@@ -57,17 +103,17 @@ const Weather = () => {
             <div
               style={{
                 width: "20%",
-                height: "10%",
+                height: "100%",
                 display: "flex",
-                flexDirection: "row",
+                flexDirection: "column",
                 justifyContent: "center",
-                // backgroundColor: "red",
+                 //backgroundColor: "red",
                 borderRadius: 20,
                 alignItems: "center",
                 // paddingLeft:20
               }}
             >
-              <p>Maluti</p>
+              <p>{location}</p>
             </div>
             <div
               style={{
@@ -82,13 +128,13 @@ const Weather = () => {
                 paddingLeft: 20,
               }}
             >
-              <p>20.04.2023</p>
+              <p>{weatherData.daily && weatherData.daily.time[0]}</p>
             </div>
           </div>
           <div
             style={{
               width: "100%",
-              height: "80%",
+              height: "60%",
               display: "flex",
               flexDirection: "row",
               justifyContent: "center",
@@ -138,7 +184,7 @@ const Weather = () => {
                   alignItems: "center",
                 }}
               >
-                20
+            {weatherData.daily && weatherData.daily.temperature_2m_max[0]}
                 <span style={{ fontSize: "40px", alignSelf: "flex-start" }}>
                   °C
                 </span>
@@ -157,8 +203,8 @@ const Weather = () => {
                  borderRadius: 20,
               }}
             >
-              <p>wind Speed: 2.4 km/h</p>
-              <p>Humidity: 60%</p>
+              <p>wind Speed:{weatherData.daily && weatherData.daily.windspeed_10m_max[0]} km/h</p>
+              <p>Humidity: {weatherData.daily.precipitation_probability_max[0]}%</p>
             </div>
           </div>
         </div>
